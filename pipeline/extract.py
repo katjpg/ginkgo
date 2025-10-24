6081cdfc60ce523470a5572ab2ddfc949fbd356e
katjpg <63516371+katjpg@users.noreply.github.com>
2025-10-24 03:20:33 -0700

    documenting tentative TODOs

---
+++ pipeline/extract.py
@@ -0,0 +1,1 @@
+# TODO: extraction pipeline (wip)
---
+++ utils/format_text.py
@@ -0,0 +1,5 @@
+# TODO: text formatter to concatenate title + text for langextract extraction
+# def format_text
+# include start_char and end_char
+# detect boundaries; interval array
+# section_boundaries = [ {'title': 'Introduction}, 'start_char': 0, 'end_char': 0]} , ... ]
